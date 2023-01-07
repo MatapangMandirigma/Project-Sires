8f3832a5647ea5301dc6428e8ddab90c9ddd88c7
MatapangMandirigma <bma190000@utdallas.edu>
2023-01-06 22:00:03 -0600

    Hamburger and header

--- main/homepage/homepage.js
+++ main/homepage/homepage.js
@@ -1,1 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navShade = document.querySelector(".nav-shade");
+//const navBar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", menu);
+navShade.addEventListener("click", closeMenu);
+//navBar.addEventListener("click", closeMenu);
+
+function menu(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navShade.classList.toggle("active");
+}
+
+function closeMenu(){
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navShade.classList.remove("active");
+}
+
 
