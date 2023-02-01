cfe334f6cfe4ca256eaae6117c71fc1feae92de8
MatapangMandirigma <bma190000@utdallas.edu>
2023-02-01 15:00:47 -0600

    Fixed buttons to now work in .js file They now can go to any url necessary

--- main/homepage/homepage.js
+++ main/homepage/homepage.js
@@ -1,10 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navShade = document.querySelector(".nav-shade");
+const homeBtn = document.querySelector(".home");
+const projectsBtn = document.querySelector(".projects");
+const aboutBtn = document.querySelector(".about");
+const bBtn = document.querySelector(".b");
+const nBtn = document.querySelector(".n");
 //const navBar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", menu);
 navShade.addEventListener("click", closeMenu);
+homeBtn.addEventListener("click", function(){ btnLink('home') });
+projectsBtn.addEventListener("click", function(){ btnLink('projects') });
+aboutBtn.addEventListener("click", function(){ btnLink('about') });
+bBtn.addEventListener("click", function(){ btnLink('b') });
+nBtn.addEventListener("click", function(){ btnLink('n') });
 //navBar.addEventListener("click", closeMenu);
 
 function menu(){
@@ -19,4 +29,28 @@ function closeMenu(){
     navShade.classList.remove("active");
 }
 
+function btnLink(btn) {
+    var btnURL = '';
+    switch(btn){
+        case 'home':
+            btnURL = 'homepage.html';
+            break;
+        case 'projects':
+            btnURL = 'homepage.html#projects';
+            break;
+        case 'about':
+            btnURL = 'homepage.html#about';
+            break;
+        case 'b':
+            btnURL = 'homepage.html#b';
+            break;
+        case 'n':
+            btnURL = 'homepage.html#n';
+            break;
+        default:
+            btnURL = 'homepage.html#';
+    }
 
+    window.location.href = btnURL;
+    closeMenu();
+}
